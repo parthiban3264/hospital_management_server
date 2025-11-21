38922d87b87c059cd652f40442b996d73466635d
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-11-21 18:20:11 +0530

    21-11-2025

--- src/AdminStrator/adminstrator.module.ts
+++ src/AdminStrator/adminstrator.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
-import { AdminStratorService } from "./Adminstrator.Service";
+import { AdminStratorService } from "./AdminStrator.Service";
 import { AdminStratorController } from "./AdminStrator.Controller";
 
 @Module({
