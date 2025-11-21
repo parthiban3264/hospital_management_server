91b0bfc891641f82a9d695c28588ca6901fb186f
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-11-21 18:31:32 +0530

    21-11-25

--- src/AdminStrator/adminstrator.module.ts
+++ src/AdminStrator/adminstrator.module.ts
@@ -6,6 +6,5 @@ import { AdminStratorController } from "./AdminStrator.Controller";
 @Module({
   controllers: [AdminStratorController],
   providers: [AdminStratorService, PrismaService],
-  exports: [AdminStratorService], // optional if other modules need it
 })
 export class AdminStratorModule {}
