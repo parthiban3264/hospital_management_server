20ce383fd279471a7837413ac7d7ed27ccaa6ed3
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-11-21 18:30:31 +0530

    21-11-25

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { MedicineTonicInjectionModule } from './MedicineTonicInjection/medicineT
 import { ScanAndTestModule } from './TestAndScan/test-scan.module';
 import { DrawerModule } from './Drawer/drawer.module';
 import { IncomeAndExpenseModule } from './IncomeExpense/incomeExpense.module';
-import {AdminStratorModule  } from './AdminStrator/Adminstrator.module';
+import {AdminStratorModule  } from './AdminStrator/AdminStrator.module';
 @Module({
   imports: [HospitalModule,
     UserModule,
