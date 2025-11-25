2ea0714538173fdb1a6f89371d464b065b7a3eb3
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-11-25 17:59:05 +0530

    25-11-2025 hospitalimage

--- src/Hospitals/hospital.controller.ts
+++ src/Hospitals/hospital.controller.ts
@@ -138,7 +138,7 @@ async updateWithFile(
   // Replace image only if new file is uploaded
   if (file) {
     imageUrl = `https://hospitalservers.ramchintech.com/hospital_images/${id}/${file.filename}`;
-    return imageUrl;
+   
 
     // Delete old image if exists
     if (oldImage) {
@@ -149,6 +149,7 @@ async updateWithFile(
 
       if (fs.existsSync(oldLocalPath)) {
         fs.unlinkSync(oldLocalPath);
+        return 'Old image deleted';
       }
     }
   }
