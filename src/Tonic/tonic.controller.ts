2629d899b9aa346d17fd79547453ef83063d68e3
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-12-02 22:41:40 +0530

    2-12-2004

--- src/Tonic/tonic.controller.ts
+++ src/Tonic/tonic.controller.ts
@@ -32,7 +32,7 @@ export class TonicController {
     return this.tonicService.findByHospitalAndName(Number(hospitalId), name);
   }
 
-  @Patch('updateById:id')
+  @Patch('updateById/:id')
   update(@Param('id') id: string, @Body() data: any) {
     return this.tonicService.update(+id, data);
   }
