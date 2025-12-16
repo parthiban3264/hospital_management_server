a475089fdc176b58bc6b1d590b2d116df296455a
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-12-16 12:29:40 +0530

    16-12-2004

--- src/Payments/payment.service.ts
+++ src/Payments/payment.service.ts
@@ -108,7 +108,7 @@ async findPaidByHospitalAccounts(hospitalId: number) {
     },
     include: {
       Hospital: {select: {id:true ,name: true,address:true,},},
-      Patient: {select: { hospital_Id:true,user_Id: true, name:true, dob:true, gender:true,phone:true,address:true,createdAt:true,},},
+      Patient: {select: { id:true,hospital_Id:true,user_Id: true, name:true, dob:true, gender:true,phone:true,address:true,createdAt:true,},},
       Consultation: {select:{ id : true ,doctor_Id:true,patient_Id:true,} },
       TestingAndScanningPatients: {select: { id: true, title: true, type: true, status: true,payment_Id:true, consultation_Id: true,amount:true },},
       MedicinePatient: {select: { id: true, medicine_Id: true, quantity: true,payment_Id:true, consultation_Id: true,total:true },},
