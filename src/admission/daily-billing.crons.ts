0591a046ee234fc376956a2da9e4535b22d9840f
parthiban3264 <parthiban.tcr.2004@gmail.com>
2026-01-29 18:17:15 +0530

    29-1-2026 600

--- src/admission/daily-billing.crons.ts
+++ src/admission/daily-billing.crons.ts
@@ -107,19 +107,23 @@ export class AdmissionServiceCron {
   // async createDailyPayment() {
   //   await this.AdmissionService.createDailyPayment();
   // }
- @Cron('*/30 * 18 40 * *')
- // @Cron('*/30 * * * * *')
-  async createDailyPayment1() {
-    await this.AdmissionService.createDailyPayment();
-  }
-  @Cron('*/30 * 18 50 * *')
- // @Cron('*/30 * * * * *')
-  async createDailyPayment2() {
-    await this.AdmissionService.createDailyPayment();
-  }
-    @Cron('*/30 * 19 * * *')
- // @Cron('*/30 * * * * *')
-  async createDailyPayment3() {
-    await this.AdmissionService.createDailyPayment();
-  }
+
+  // 18:40 every day
+@Cron('0 40 18 * * *')
+async createDailyPayment1840() {
+  await this.AdmissionService.createDailyPayment();
+}
+
+// 18:50 every day
+@Cron('0 50 18 * * *')
+async createDailyPayment1850() {
+  await this.AdmissionService.createDailyPayment();
+}
+
+// 19:00 every day
+@Cron('0 0 19 * * *')
+async createDailyPayment1900() {
+  await this.AdmissionService.createDailyPayment();
+}
+
 }
